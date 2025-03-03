a39d583f4e8daa40f8c0ccb65eca6850b76f841a
bbang427 <seungwonbang91@gmail.com>
2025-03-03 19:39:33 +0900

    Update a.java

--- java_test/a.java
+++ java_test/a.java
@@ -1,9 +1,23 @@
 package java_test;
+import java.util.Scanner;
 
 public class a {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        int[] arr = new int[input.length()];
+
+        for (int i = 0; i < input.length(); i++) {
+            arr[i] = input.charAt(i) - '0';
+        }
+        
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+
 
     }
 
