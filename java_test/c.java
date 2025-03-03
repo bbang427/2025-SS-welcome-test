12d7676b1c9dec0613171774ce568dee0348fa36
bbang427 <seungwonbang91@gmail.com>
2025-03-03 19:41:52 +0900

    Update c.java

--- java_test/c.java
+++ java_test/c.java
@@ -1,9 +1,18 @@
 package java_test;
+import java.util.Scanner;
+
 
 public class c {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner scanner = new Scanner(System.in);
+        
+        int age=scanner.nextInt();
+        int height=scanner.nextInt();
+        
+        if(age>=14||height>=155) System.out.print('X');
+        else System.out.print('O');
+
 
     }
 
