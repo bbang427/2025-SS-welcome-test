2a35d768dcaa3a3843ef92424cbae626260a4a3a
bbang427 <seungwonbang91@gmail.com>
2025-03-03 19:43:49 +0900

    Update f.java

--- java_test/f.java
+++ java_test/f.java
@@ -1,11 +1,26 @@
 package java_test;
 
-// 이부분에 class 선언 해도 괜찮습니다
+import java.util.Scanner;
+
 
 public class f {
     public static void main(String[] args) {
 
-        // 이 부분에 code를 작성해주세요!
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<n-i;j++) System.out.print(" ");
+            for(int j=0;j<2*i-1;j++) System.out.print("*");
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=0;j<n-i;j++) System.out.print(" ");
+            for(int j=0;j<2*i-1;j++) System.out.print("*");
+            System.out.println();
+        }
+
 
     }
 
